b52c10dfc5a78673f0583a6f5d84656a3a057070
satty3104@gmail.com <satty3104@gmail.com@35014f90-9c56-c9b3-4585-c533e6891ce4>
2012-01-23 16:32:18 +0000

    ファクトリメソッドパターン適用

--- trunk/examples/test/MyStringUtilsTestNgpPoiData.java
+++ trunk/examples/test/MyStringUtilsTestNgpPoiData.java
@@ -4,21 +4,19 @@ import java.util.Iterator;
 
 import org.testng.annotations.DataProvider;
 
-import s.n.testngppoi.factory.SSFDataProviderFactory;
 import s.n.testngppoi.factory.creator.SSFDataProviderFactoryCreator;
-import s.n.testngppoi.iterator.HSSFDataProvider;
 import s.n.testngppoi.type.Type;
 
 public class MyStringUtilsTestNgpPoiData {
 
 	@DataProvider()
 	public static Iterator<Object[]> testConcatData() {
-		return new HSSFDataProvider("MyStringUtilsTestNgpPoiData.xls", "testConcatData");
+		return SSFDataProviderFactoryCreator.getInstance(Type.HSSF).create(
+				"MyStringUtilsTestNgpPoiData.xls", "testConcatData");
 	}
 
 	@DataProvider()
 	public static Iterator<Object[]> test() {
-		SSFDataProviderFactory factory = SSFDataProviderFactoryCreator.getInstance(Type.HSSF);
-		return factory.create();
+		return SSFDataProviderFactoryCreator.getInstance(Type.HSSF).create();
 	}
 }
